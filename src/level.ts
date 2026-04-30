de9780d4f2881d76dd85fc2faa9b74c43fe523ed
Masonlet <masonletoile@hotmail.com>
2026-04-30 11:08:48 -0400

    feat: checkered floor

--- src/level.ts
+++ src/level.ts
@@ -26,7 +26,9 @@ export function drawLevel(
   for (let y = 0; y < level.height; y++) {
     for (let x = 0; x < level.width; x++) {
       const id = getTile(level, x, y);
-      ctx.fillStyle = id === TILE.WALL ? "#444" : "#222";
+      ctx.fillStyle = id === TILE.WALL
+        ? "#444"
+        : (x + y) % 2 === 0 ? "#222" : "#262626";
       ctx.fillRect(
         Math.floor(x * TILE_SIZE - camX),
         Math.floor(y * TILE_SIZE - camY),
