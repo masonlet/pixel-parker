2beff0fcdedf48417d6b602d98b4cb626b123ab3
Masonlet <masonletoile@hotmail.com>
2026-05-21 17:47:47 -0400

    refactor: use Set for sensor overlap lookup

--- src/scripts/physics/sensors.ts
+++ src/scripts/physics/sensors.ts
@@ -16,6 +16,6 @@ export function vehicleObb(v: Vehicle): OBB {
 export function sensorsOverlapping(v: Vehicle, level: Level): Sensor[] {
   const obb = vehicleObb(v);
   const aabbs: AABB[] = level.sensors.map(s => ({ x: s.x, y: s.y, w: s.w, h: s.h }));
-  const hits = overlappingRegions(obb, aabbs);
-  return hits.map(hit => level.sensors[aabbs.indexOf(hit)]!);
+  const hits = new Set(overlappingRegions(obb, aabbs));
+  return level.sensors.filter((_, i) => hits.has(aabbs[i]!));
 }
