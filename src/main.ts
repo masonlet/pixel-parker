b942668de30cbf87a6e7841f22b830c6fa24e4bb
Masonlet <masonletoile@hotmail.com>
2026-05-23 21:28:56 -0400

    chore: migrate to Audio class and fix startLoop options

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,23 @@
 import "./style.css";
 
 import { startLoop } from "web-engine/update.ts";
-import { registerSound } from "web-engine/audio/registry.ts";
+import { Audio     } from "web-engine/audio.ts";
 
-import { bootstrapGame } from "./scripts/game/game.ts";
+import { bootstrapGame   } from "./scripts/game/game.ts";
 import type { FrameState } from "./scripts/game/types.ts";
 import { createPlayState } from "./scripts/game/play.ts";
 import { updateFrame, renderFrame } from "./scripts/game/frame.ts";
 
 import { loadCampaign } from "./scripts/campaign/load.ts";
 
+const BASE_URL = import.meta.env.BASE_URL;
+
 const { canvas, ctx } = bootstrapGame();
 
-const BASE_URL = import.meta.env.BASE_URL;
-await registerSound("button", "audio/ui/button.wav", BASE_URL);
-await registerSound("win", "audio/ui/win.wav", BASE_URL);
+const audio = new Audio();
+audio.init();
+await audio.registerSound("button", "audio/ui/button.wav", BASE_URL);
+await audio.registerSound("win", "audio/ui/win.wav", BASE_URL);
 
 const campaign = await loadCampaign("test");
 const playState = createPlayState(campaign);
@@ -22,6 +25,6 @@ let frame: FrameState = { game: "menu-title", ui: null };
 
 startLoop(
   (dt) => { frame = updateFrame(canvas, frame, playState, dt); },
-  (  ) => { renderFrame(ctx, canvas, playState, frame);       },
-  { tickRate: "variable" },
+  (  ) => { renderFrame(ctx, canvas, playState, frame);        },
+  { tickRate: "variable", maxDelta: 250 },
 );
